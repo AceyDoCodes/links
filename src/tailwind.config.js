bc1b88dfdfe19d021276899343f7160f9367bd55
William Harrison <william@williamdavidharrison.com.au>
2022-08-17 17:58:07 +0800

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -8,7 +8,8 @@ tailwind.config = {
                 "accent": "#DC3962",
                 "lightAccent": "#FF715B",
                 "primaryText": "#FFFFFF",
-                "secondaryText": "#6B8594"
+                "secondaryText": "#6B8594",
+                "text-accent": "#22242d"
             }
         }
     }
