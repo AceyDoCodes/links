51216e02d5129ca61ae0399a8ceb0162745e4fcf
William Harrison <william@williamdavidharrison.com.au>
2022-08-17 17:54:00 +0800

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -5,7 +5,7 @@ tailwind.config = {
                 "backgroundColor": "#12141D",
                 "backgroundAccent": "#1E2029",
                 "backgroundAccentLighter": "#22242d",
-                "accent": "#DC3962",
+                "accent": "#22242d",
                 "lightAccent": "#FF715B",
                 "primaryText": "#FFFFFF",
                 "secondaryText": "#6B8594"
