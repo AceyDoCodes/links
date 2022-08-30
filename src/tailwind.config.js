d2bd46d547f4b648c03ef269a4dffcbabf060cb0
William Harrison <william@williamdavidharrison.com.au>
2022-08-30 10:46:58 +0800

    try 12px

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -19,7 +19,7 @@ tailwind.config = {
             "md": "0.375rem",
             "lg": "0.5rem",
             "full": "9999px",
-            "social": "6px",
+            "social": "12px",
             "other": "6px",
             "link": "12px"
         }
