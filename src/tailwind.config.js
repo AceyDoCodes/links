077f443fcae74708e557c694e5075d9e1a13189f
William Harrison <william@williamdavidharrison.com.au>
2022-08-30 10:43:10 +0800

    change again

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -19,7 +19,7 @@ tailwind.config = {
             "md": "0.375rem",
             "lg": "0.5rem",
             "full": "9999px",
-            "social": "25%",
+            "social": "35%",
             "link": "12px"
         }
     }
